e292aafcdc1eb4201f97f864119cfff1f9ed2acb
Prashant Khanal <prashantkhanal32@gmail.com>
2021-08-07 17:54:27 +0545

    final commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,33 @@
+/* eslint-disable vue/require-v-for-key */
 <template>
   <div>
-    <h1>{{ data }}</h1>
-    <button @click="add()">Increment</button>
-    <button @click="decrement()">Decrese</button>
-    <h3>{{ this.counter }}</h3>
-
-    <form @submit.prevent="formSubmit">
-      <input type="text" v-model="name" />
-      <button>Submit</button>
-    </form>
+    <h1>Vue Goals</h1>
+    <div>
+      <h5>my course goals</h5>
+      <input type="text" v-model="enteredGoals" />
+      <button @click="addGoals">Add Goal</button>
+      <p v-if="goals.length === 0">
+        Ni goals have been added yet please start adding some!
+      </p>
+      <ul v-else>
+        <li
+          v-for="(goal, index) in goals"
+          :key="goal"
+          @click="removedGoals(index)"
+        >
+          {{ goal }} - {{ index }}
+        </li>
+      </ul>
+      <h6>This is for the object</h6>
+      <ul>
+        <li v-for="(value, key, index) in discriptions" :key="index">
+          value: {{ value }} <br />
+          key: {{ key }}
+          <br />
+          index:{{ index }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
@@ -16,31 +35,20 @@ export default {
   name: 'Home',
   data() {
     return {
-      counter: 0,
-      name: '',
+      goals: [],
+      enteredGoals: '',
+      discriptions: {
+        firstName: 'prashant',
+        lastName: 'khanal',
+      },
     };
   },
-  props: {
-    data: String,
-  },
-  watch: {
-    counter(value) {
-      if (value > 5) {
-        this.counter = 0;
-      }
-    },
-  },
   methods: {
-    formSubmit() {
-      alert('form submitted');
-      //
+    addGoals() {
+      this.goals.push(this.enteredGoals);
     },
-
-    add() {
-      this.counter = this.counter + 1;
-    },
-    decrement() {
-      this.counter = this.counter - 1;
+    removedGoals(idx) {
+      this.goals.splice(idx, 1);
     },
   },
 };
